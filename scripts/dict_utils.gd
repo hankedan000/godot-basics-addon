8918bd8eae2e8f24ce3c8902ba69a7b994b566a4
hankedan000 <dany32412@gmail.com>
2025-07-28 07:15:06 -0400

    Add enum lookup methods to DictUtils

--- scripts/dict_utils.gd
+++ scripts/dict_utils.gd
@@ -5,3 +5,10 @@ static func get_w_default(dict: Dictionary, key: Variant, default_value=null):
 	if dict.has(key):
 		return dict[key]
 	return default_value
+
+static func put_enum(dict: Dictionary, key: Variant, enum_class, enum_value):
+	dict[key] = EnumUtils.to_str(enum_class, enum_value)
+
+static func get_enum_w_default(dict: Dictionary, key: Variant, enum_class, def_value):
+	var enum_value_str := get_w_default(dict, key, "") as String
+	return EnumUtils.from_str(enum_class, def_value, enum_value_str)
